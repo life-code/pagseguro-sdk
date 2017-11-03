481826ae9dc7c4a85000708e8d894b9347d451bc
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-03 13:18:11 -0200

    Create PreApproval.php

---
+++ src/PreApprovals/PreApproval.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pagseguro\PreApprovals;
+
+class PreApproval
+{
+    /**
+     * Approves one payment
+     * 
+     * @param Customer $customer
+     * @return App\Pagseguro\Responses\ResponseInterface
+     */
+    public function approve(Customer $customer)
+    {
+        // 
+    }
+}

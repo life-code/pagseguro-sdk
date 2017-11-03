b253fd3f55af63890245f77feeb0d080d79725e7
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-03 09:43:42 -0200

    Update Pagsseguro.php

--- src/Pagsseguro.php
+++ src/Pagsseguro.php
@@ -4,5 +4,20 @@ namespace Pagseguro;
 
 class Paguseguro
 {
+    /**
+     * The Pagseguro version.
+     *
+     * @var string
+     */
+    const VERSION = '0.1';
 
+    /**
+     * Get the version number of the application.
+     *
+     * @return string
+     */
+    public function version()
+    {
+        return static::VERSION;
+    }
 }

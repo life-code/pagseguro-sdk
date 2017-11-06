15e6a9acd4537df49731d729755506434abaa9b8
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-06 09:48:40 -0200

    Update Phone.php

--- src/Contracts/Phone.php
+++ src/Contracts/Phone.php
@@ -1,7 +1,43 @@
 <?php
+
 namespace PagSeguro\Contracts;
 
 interface Phone
 {
-    // 
+    /**
+     * Get code
+     * 
+     * @return string
+     */
+    public function getAreaCode() : string
+
+    /**
+     * Set code
+     * 
+     * @param string $code
+     * @return $this
+     */
+    public function setAreaCode(string $area_code)
+
+    /**
+     * Get number
+     * 
+     * @return string
+     */
+    public function getNumber() : string
+
+    /**
+     * Set number
+     * 
+     * @param string $number
+     * @return string
+     */
+    public function setNumber(string $number)
+    
+    /**
+     * To string phone
+     * 
+     * @return string
+     */
+    public function toString() : string
 }

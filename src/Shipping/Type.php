20ffa2f96b81bce913daf0971912e98a36ce9533
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-06 17:53:54 +0000

    Update Type.php

--- src/Shipping/Type.php
+++ src/Shipping/Type.php
@@ -4,5 +4,43 @@ namespace PagSeguro\Shipping;
 
 class Type
 {
-    // 
+    /**
+     * @var int
+     */
+    const TYPE_PAC = 1;
+    
+    /**
+     * @var int
+     */
+    const TYPE_SEDEX = 2;
+    
+    /**
+     * @var int
+     */
+    const TYPE_UNKNOWN = 3;
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes() : array
+    {
+        return [
+            self::TYPE_PAC, 
+            self::TYPE_SEDEX, 
+            self::TYPE_UNKNOWN
+        ];
+    }
+    
+    /**
+     * Check type exists
+     * 
+     * @param int $type
+     * @return bool
+     */
+    public static function exists(int $type) : bool
+    {
+        return in_array($type, self::getTypes());
+    }
 }

4eeb31145c966ee3a38fa6242917bfbee18cfb86
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-06 09:51:34 -0200

    Update Address.php

--- src/Contracts/Address.php
+++ src/Contracts/Address.php
@@ -4,5 +4,123 @@ namespace PagSeguro\Contracts;
 
 interface Address
 {
-    // 
+    /**
+     * Get country
+     * 
+     * @return string
+     */
+    public function getCountry() : string
+
+    /**
+     * Set country
+     * 
+     * @param string $country
+     * @return $this
+     */
+    public function setCountry(string $country)
+
+    /**
+     * Get state
+     * 
+     * @return string
+     */
+    public function getState() : string
+
+    /**
+     * Set state
+     * 
+     * @param string $state
+     * @return $this
+     */
+    public function setState(string $state)
+
+    /**
+     * Get city
+     * 
+     * @return string
+     */
+    public function getCity() : string
+
+    /**
+     * Set city
+     * 
+     * @param string $city
+     * @return $this
+     */
+    public function setCity(string $city)
+
+    /**
+     * Get cep
+     * 
+     * @return string
+     */
+    public function getCep() : string
+
+    /**
+     * Set cep
+     * 
+     * @param string $cep
+     * @return $this
+     */
+    public function setCep(string $cep)
+
+    /**
+     * Get district
+     * 
+     * @return string
+     */
+    public function getDistrict() : string
+
+    /**
+     * Set district
+     * 
+     * @param string $district
+     * @return $this
+     */
+    public function setDistrict(string $district)
+
+    /**
+     * Get street
+     * 
+     * @return string
+     */
+    public function getStreet() : string
+
+    /**
+     * Set street
+     * 
+     * @param string $street
+     * @return $this
+     */
+    public function setStreet(string $street)
+
+    /**
+     * Get number
+     * 
+     * @return string
+     */
+    public function getNumber() : string
+
+    /**
+     * Set number
+     * 
+     * @param string $number
+     * @return $this
+     */
+    public function setNumber(string $number)
+
+    /**
+     * Get complement
+     * 
+     * @return string
+     */
+    public function getComplement() : string
+
+    /**
+     * Set complement
+     * 
+     * @param string $complement
+     * @return $this
+     */
+    public function setComplement(string $complement)
 }

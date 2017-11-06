7075b6c759144e0d1c86729d74368e47115534d4
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-06 09:43:06 -0200

    Update Address.php

--- src/Customer/Address.php
+++ src/Customer/Address.php
@@ -4,5 +4,43 @@ namespace PagSeguro\Customer;
 
 class Address
 {
-    // 
+    /**
+     * @var string
+     */
+    private $country = 'BRA';
+
+    /**
+     * @var string
+     */
+    private $state;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var string
+     */
+    private $cep;
+
+    /**
+     * @var string
+     */
+    private $district;
+
+    /**
+     * @var string
+     */
+    private $street;
+
+    /**
+     * @var string
+     */
+    private $number;
+
+    /**
+     * @var string
+     */
+    private $complement;
 }

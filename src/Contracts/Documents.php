1bc7a63eb820a5a6926a19d9915725afa16b99fa
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-06 09:45:39 -0200

    Update Documents.php

--- src/Contracts/Documents.php
+++ src/Contracts/Documents.php
@@ -3,5 +3,40 @@ namespace PagSeguro\Contracts;
 
 interface Documents
 {
-    // 
+    /**
+     * Get item
+     * 
+     * @return string
+     */
+    public function getItem($type)
+    
+    /**
+     * Set CPF
+     * 
+     * @param string $cpf
+     * @return $this
+     */
+    public function setItem(string $type, string $item)
+    
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public function validate(string $type) : bool
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public function getTypes()
+    
+    /**
+     * Validate phone
+     * 
+     * @return bool
+     */
+    public function isValid()
 }

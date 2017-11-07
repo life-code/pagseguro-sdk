23aae9210e6cf40e871f51381eaffcb99b611a4a
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-07 15:03:06 -0200

    Update Response.php

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -6,5 +6,133 @@ use PagSeguro\Contracts\Http\Response as ResponseContract;
 
 abstract class Response implements ResponseContract
 {
+    /**
+     * @var int
+     */
+    protected $status;
     
+    /**
+     * @var array
+     */
+    protected $data = [];
+    
+    /**
+     * @var array
+     */
+    protected $info;
+    
+    /**
+     * @var array
+     */
+    protected $errors;
+    
+    /**
+     * Get status
+     * 
+     * @return int
+     */
+    public function getStatus() : int
+    {
+        return $this->status;
+    }
+    
+    /**
+     * Set status
+     * 
+     * @param int $status
+     * @return $this
+     */
+    public function setStatus(int $status)
+    {
+        $this->status = $status;
+        
+        return $this;
+    }
+    
+    /**
+     * Get all data
+     * 
+     * @return array
+     */
+    public function all() : array
+    {
+        return $this->data;
+    }
+    
+    /**
+     * Get info
+     * 
+     * @return array
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+    
+    /**
+     * Set info
+     * 
+     * @param array $info
+     * @return array
+     */
+    public function setInfo(array $info)
+    {
+        $this->info = $info;
+        
+        return $this;
+    }
+    
+    /**
+     * Get errors
+     * 
+     * @return array
+     */
+    public function getErrors() : array
+    {
+        return $this->errors;
+    }
+    
+    /**
+     * Has errors
+     * 
+     * @return bool
+     */
+    public function hasErrors() : bool
+    {
+        return $this->getErrors() ? true : false;
+    }
+    
+    /**
+     * Set errors
+     * 
+     * @param mixed $errors
+     * @return $this
+     */
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
+        
+        return $this;
+    }
+
+    /**
+     * Proxy accessing an attribute onto the response data.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function __get(string $key)
+    {
+        return $this->data->$key;
+    }
+
+    /**
+     * Proxy accessing an attribute onto the response data.
+     *
+     * @return json
+     */
+    public function __toString()
+    {
+        return json_encode($this->data);
+    }
 }

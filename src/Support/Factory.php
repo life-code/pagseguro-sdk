f5ba4a8e94270786399574c9843b39ee8f8f13e7
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-07 19:03:12 +0000

    Create Support Factory

---
+++ src/Support/Factory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PagSeguro\Support;
+
+use PagSeguro\Credentials\AccountCredentials;
+use PagSeguro\Credentials\Environment;
+
+trait Factory
+{
+    /**
+     * @var \PagSeguro\Credentials\Environment
+     */ 
+    private $env;
+    
+    /**
+     * @var \PagSeguro\Credentials\AccountCredentials
+     */ 
+    private $credentials;
+    
+    /**
+     * Get env
+     * 
+     * return \PagSeguro\Credentials\Environment
+     */ 
+    public static function getEnv()
+    {
+        if (!self::$env) {
+            self::$env = new Environment();
+        }
+        
+        return self::$env;
+    }
+    
+    /**
+     * Get credentials
+     * 
+     * return \PagSeguro\Credentials\AccountCredentials
+     */ 
+    public static function getCredentials()
+    {
+        if (!self::$credentials) {
+            $env = $this->getEnv();
+            self::$credentials = new AccountCredentials($env->getEmail(), $env->getToken());
+        }
+        
+        return self::$credentials;
+    }
+}

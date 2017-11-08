26429c0f07690219e2b0e95f0301418026380e15
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-08 12:24:59 +0000

    Update Documents.php

--- src/Customer/Documents.php
+++ src/Customer/Documents.php
@@ -42,6 +42,7 @@ class Documents implements DocumentsContract
      * 
      * @param string $cpf
      * @param string $item
+     * @throws \PagSeguro\Exceptions\PagseguroException
      * @return $this
      */ 
     public function setItem(string $type, string $item)

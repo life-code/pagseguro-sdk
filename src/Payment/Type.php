8b4c87d5389f383ba5e0eafa985a50ed5dfc6632
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-08 11:48:02 +0000

    Create Type.php

---
+++ src/Payment/Type.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PagSeguro\Payment;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.6
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Type
+{
+	// 
+}

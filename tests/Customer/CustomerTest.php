1c9a3c3f8831dc3d05f115dd6ed4fc9b25de4e8a
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-09 18:41:55 +0000

    Create tests for getters e setters Customer

--- tests/Customer/CustomerTest.php
+++ tests/Customer/CustomerTest.php
@@ -4,9 +4,20 @@ use PHPUnit\Framework\TestCase;
 
 use PagSeguro\Contracts\Customer as CustomerContract;
 use PagSeguro\Customer\Customer;
+use PagSeguro\Exceptions\PagseguroException;
 
 class CustomerTest extends TestCase
 {
+    /**
+     * Customer Instance
+     * 
+     * @return \PagSeguro\Contracts\Customer
+     */
+    public function instance()
+    {
+        return new Customer();
+    }
+    
     /**
      * Test instance
      *
@@ -14,6 +25,37 @@ class CustomerTest extends TestCase
      */
     public function testInstance()
     {
-        $this->assertInstanceOf(CustomerContract::class, new Customer());
+        $this->assertInstanceOf(CustomerContract::class, $this->instance());
+    }
+    
+    /**
+     * Test instance
+     *
+     * @return void
+     */
+    public function testSetEmail()
+    {
+        $this->assertInstanceOf(CustomerContract::class, $this->instance()->setEmail('vinicius_puglies@outlook.com'));
+    }
+    
+    /**
+     * Test instance
+     *
+     * @return void
+     */
+    public function testGetEmail()
+    {
+        $this->assertEquals('vinicius_puglies@outlook.com', $this->instance()->setEmail('vinicius_puglies@outlook.com')->getEmail());
+    }
+    
+    /**
+     * Test instance
+     *
+     * @expectedException \PagSeguro\Exceptions\PagseguroException
+     * @return void
+     */
+    public function testTrowSetEmail()
+    {
+        $this->instance()->setEmail('vinicius_puglies');
     }
 }

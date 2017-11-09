61453e3bcc94384272681065b87da4c60166d3be
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-09 13:01:32 +0000

    Create helpers file

---
+++ src/Support/helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        
+        if ($value === false) {
+            return $default;
+        }
+        
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        
+        if (strlen($value) > 1 && substr($value, 0, 1) === '"' && substr($value, -1) === '"') {
+            return substr($value, 1, -1);
+        }
+        
+        return $value;
+    }
+}

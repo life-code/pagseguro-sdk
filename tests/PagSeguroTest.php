6585ac35d2b2f82061869edc31871d73c730506f
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-09 13:01:47 +0000

    Create PagseguroTest

---
+++ tests/PagSeguroTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\PagSeguro;
+use PagSeguro\Session\Session;
+use PagSeguro\PreApprovals\PreApproval;
+
+class PagSeguroTest extends TestCase
+{
+    /**
+     * Test version
+     *
+     * @return void
+     */
+    public function testVersion()
+    {
+        $this->assertEquals('0.7', PagSeguro::version());
+    }
+    
+    /**
+     * Test instance session
+     *
+     * @return void
+     */
+    public function testSession()
+    {
+        $this->assertInstanceOf(Session::class, PagSeguro::session());
+    }
+    
+    /**
+     * Test instance session
+     *
+     * @return void
+     */
+    public function testPreApproval()
+    {
+        $this->assertInstanceOf(PreApproval::class, PagSeguro::preApproval());
+    }
+}

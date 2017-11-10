f71d155d70c3a9a35c54d1758176e34e3b672c1f
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-10 12:39:15 +0000

    Create Environment contract class with methods

---
+++ src/Contracts/Environment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace PagSeguro\Contracts;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.3
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Environment
+{
+    /**
+     * Get environment
+     * 
+     * @return string
+     */
+    public function getEnv() : string;
+
+    /**
+     * Get email
+     * 
+     * @return string
+     */
+    public function getEmail() : string;
+
+    /**
+     * Get token
+     * 
+     * @return string
+     */
+    public function getToken() : string;
+
+    /**
+     * Get token
+     * 
+     * @return string
+     */
+    public function getAppID() : string;
+
+    /**
+     * Get token
+     * 
+     * @return string
+     */
+    public function getAppKey() : string;
+
+    /**
+     * Get URL
+     * 
+     * @return string
+     */
+    public function getUrl() : string;
+
+    /**
+     * Get token
+     * 
+     * @return string
+     */
+    public function getScript() : string;
+}

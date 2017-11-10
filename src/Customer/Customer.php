8228ab1598b3ba20e2c44bd8f6da299661ec5da5
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-10 15:59:04 +0000

    Update version

--- src/Customer/Customer.php
+++ src/Customer/Customer.php
@@ -13,7 +13,7 @@ use PagSeguro\Support\Validator;
  * PagSeguro SDK
  * 
  * @type        library
- * @version     0.8.4
+ * @version     0.8.5
  * @package     life-code/pagseguro-sdk
  * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
  * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>

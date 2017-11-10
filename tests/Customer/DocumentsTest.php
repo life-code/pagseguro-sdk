29e5d04653bfd31485fb6be4b21194209d9ca16f
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-11-10 11:27:34 +0000

    Finished Documents tests

---
+++ tests/Customer/DocumentsTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Contracts\Documents as DocumentsContract;
+use PagSeguro\Customer\Documents;
+use PagSeguro\Exceptions\PagseguroException;
+
+class DocumentsTest extends TestCase
+{
+    /**
+     * Documents Instance
+     * 
+     * @return \PagSeguro\Contracts\Documents
+     */
+    public function instance()
+    {
+        return new Documents();
+    }
+    
+    /**
+     * Test set area code
+     *
+     * @return void
+     */
+    public function testSetItem()
+    {
+        $this->assertInstanceOf(DocumentsContract::class, $this->instance()->setItem(DocumentsContract::CPF, '44827813809'));
+    }
+    
+    /**
+     * Test get area code
+     *
+     * @return void
+     */
+    public function testGetItem()
+    {
+        $this->assertEquals(
+            '44827813809', 
+            $this->instance()->setItem(DocumentsContract::CPF, '44827813809')->getItem(DocumentsContract::CPF)
+        );
+    }
+    
+    /**
+     * Test throw set area code
+     *
+     * @expectedException \PagSeguro\Exceptions\PagseguroException
+     * @return void
+     */
+    public function testThrowSetItem()
+    {
+        $this->instance()->setItem('RG', '44827813809');
+    }
+}

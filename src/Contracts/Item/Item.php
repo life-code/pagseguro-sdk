2e3b01e172db7ebe4747f22e419149db244b863d
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-11 17:16:12 +0000

    Create ItemContract

---
+++ src/Contracts/Item/Item.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PagSeguro\Contracts\Items;
+
+interface Item
+{
+    /**
+     * Get ID
+     * 
+     * @return string
+     */
+    public function getId() : string;
+
+    /**
+     * Set id
+     * 
+     * @param string $id
+     * @return $this
+     */
+    public function setId(string $id);
+    
+    /**
+     * Get description
+     * 
+     * @return string
+     */
+    public function getDescription() : string;
+
+    /**
+     * Set description
+     * 
+     * @param string $description
+     * @return $this
+     */
+    public function setDescription(string $description);
+    
+    /**
+     * Get amount
+     * 
+     * @return float
+     */
+    public function getAmount() : float;
+
+    /**
+     * Set amount
+     * 
+     * @param float $amount
+     * @return $this
+     */
+    public function setAmount(float $amount);
+    
+    /**
+     * Get quantity
+     * 
+     * @return int
+     */
+    public function getQuantity() : int;
+
+    /**
+     * Set quantity
+     * 
+     * @param int $quantity
+     * @return $this
+     */
+    public function setQuantity(int $quantity);
+}

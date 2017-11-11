491740d5be729d16fea7c4c0a46a9e26b5ceeea1
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-11 18:00:10 +0000

    Create tests for getters e setters Method creditCard

--- tests/Payment/MethodTest.php
+++ tests/Payment/MethodTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 use PagSeguro\Payment\Method;
+use PagSeguro\Payment\CreditCard\CreditCard;
 
 class MethodTest extends TestCase
 {
@@ -27,7 +28,7 @@ class MethodTest extends TestCase
     }
     
     /**
-     * Test get Type
+     * Test get type
      *
      * @return void
      */
@@ -37,12 +38,38 @@ class MethodTest extends TestCase
     }
     
     /**
-     * Test set Type
+     * Test set type
      *
      * @return void
      */
     public function testSetType()
     {
         $this->assertInstanceOf(Method::class, $this->instance()->setType('CREDITCARD'));
+    }
+    
+    /**
+     * Test get credit card
+     *
+     * @return void
+     */
+    public function testGetCreditCard()
+    {
+        $this->assertInstanceOf(
+            CreditCard::class, 
+            $this->instance()->setCreditCard(new CreditCard())->getCreditCard()
+        );
+    }
+    
+    /**
+     * Test set credit card
+     *
+     * @return void
+     */
+    public function testSetCreditCard()
+    {
+        $this->assertInstanceOf(
+            Method::class, 
+            $this->instance()->setCreditCard(new CreditCard())
+        );
     }
 }

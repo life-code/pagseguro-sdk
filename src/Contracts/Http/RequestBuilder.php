1de0c750df819beefda7d44dd3197092c2714b17
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-11 17:04:01 +0000

    Create RequestBuilderContract

---
+++ src/Contracts/Http/RequestBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PagSeguro\Contracts\Http;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.7
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface RequestBuilder
+{
+    /**
+     * Send request
+     * 
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function send();
+}

d573d626b9a50b1e6088df00292d71d30f262372
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-13 19:13:49 +0000

    Add attributes in Payment

--- src/Payment/Payment.php
+++ src/Payment/Payment.php
@@ -2,6 +2,12 @@
 
 namespace PagSeguro\Payment;
 
+use PagSeguro\Contracts\Credentials\AccountCredentials;
+use PagSeguro\Contracts\Credentials\Environment;
+use PagSeguro\Http\Payment\Request;
+use PagSeguro\Exceptions\PagseguroException;
+use PagSeguro\Support\Validator;
+
 /**
  * PagSeguro SDK
  * 
@@ -14,4 +20,53 @@ namespace PagSeguro\Payment;
  */
 class Payment
 {
+    use Validator;
+    
+    /**
+     * @var \PagSeguro\Contracts\Credentials\AccountCredentials
+     */
+    private $credentials = '';
+    
+    /**
+     * @var \PagSeguro\Contracts\Credentials\Environment
+     */
+    private $env = '';
+    
+    /**
+     * @var string
+     */
+    private $mode = '';
+    
+    /**
+     * @var string
+     */
+    private $currency = '';
+    
+    /**
+     * @var string
+     */
+    private $notification_url = '';
+    
+    /**
+     * @var string
+     */
+    private $receive_email = '';
+    
+    /**
+     * @var string
+     */
+    private $reference = '';
+    
+    /**
+     * Make new instance of this class
+     * 
+     * @param \PagSeguro\Contracts\Credentials\AccountCredentials $credentials
+     * @param \PagSeguro\Contracts\Credentials\Environment $env
+     * @return void
+     */
+    public function __construct(AccountCredentials $credentials, Environment $env)
+    {
+        $this->credentials = $credentials;
+        $this->env         = $env;
+    }
 }

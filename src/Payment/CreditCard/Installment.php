6adc16c6b1be139344390cf2f6c0f16fe626266f
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-13 19:52:40 +0000

    Change comments in Installment

--- src/Payment/CreditCard/Installment.php
+++ src/Payment/CreditCard/Installment.php
@@ -25,7 +25,76 @@ class Installment
     private $no_interest_installment_quantity = 0;
     
     /**
-     * @var integer
+     * @var float
      */
     private $value = 0;
+    
+    /**
+     * Get quantity
+     * 
+     * @return integer
+     */
+    public function getQuantity() : integer
+    {
+        return $this->quantity;
+    }
+    
+    /**
+     * Set quantity
+     * 
+     * @param integer $quantity
+     * @return $this
+     */
+    public function setQuantity(integer $quantity)
+    {
+        $this->quantity = $quantity;
+        
+        return $this;
+    }
+    
+    /**
+     * Get no interest installment quantity
+     * 
+     * @return integer
+     */
+    public function getNoInterestInstallmentQuantity() : integer
+    {
+        return $this->no_interest_installment_quantity;
+    }
+    
+    /**
+     * Set no interest installment quantity
+     * 
+     * @param integer $no_interest_installment_quantity
+     * @return $this
+     */
+    public function setNoInterestInstallmentQuantity(integer $no_interest_installment_quantity)
+    {
+        $this->no_interest_installment_quantity = $no_interest_installment_quantity;
+        
+        return $this;
+    }
+    
+    /**
+     * Get value
+     * 
+     * @return float
+     */
+    public function getValue() : integer
+    {
+        return $this->value;
+    }
+    
+    /**
+     * Set value
+     * 
+     * @param float $value
+     * @return $this
+     */
+    public function setValue(float $value)
+    {
+        $this->value = $value;
+        
+        return $this;
+    }
 }

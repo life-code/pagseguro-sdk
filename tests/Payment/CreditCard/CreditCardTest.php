b7c4de781feb7e901d994282a18a14f9328de7cc
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-13 11:21:29 +0000

    Create tests for getters e setters CreditCard token

--- tests/Payment/CreditCard/CreditCardTest.php
+++ tests/Payment/CreditCard/CreditCardTest.php
@@ -25,4 +25,30 @@ class CreditCardTest extends TestCase
     {
         $this->assertInstanceOf(CreditCard::class, $this->instance());
     }
+    
+    /**
+     * Test get token
+     *
+     * @return void
+     */
+    public function testGetToken()
+    {
+        $this->assertEquals(
+            '$2y$10$fTMKmH8fmR9wUa0x35norOY46Y86T7wwsVz/0FwC7B33T.87WaFAy', 
+            $this->instance()->setToken('$2y$10$fTMKmH8fmR9wUa0x35norOY46Y86T7wwsVz/0FwC7B33T.87WaFAy')->getToken()
+        );
+    }
+    
+    /**
+     * Test set token
+     *
+     * @return void
+     */
+    public function testSetToken()
+    {
+        $this->assertInstanceOf(
+            CreditCard::class, 
+            $this->instance()->setToken('$2y$10$fTMKmH8fmR9wUa0x35norOY46Y86T7wwsVz/0FwC7B33T.87WaFAy')
+        );
+    }
 }

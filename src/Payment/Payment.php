84661fa9db8d7b7e8c81176c745cc40ca571d2bc
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-13 19:13:14 +0000

    Create Payment

---
+++ src/Payment/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PagSeguro\Payment;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.7
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Payment
+{
+}

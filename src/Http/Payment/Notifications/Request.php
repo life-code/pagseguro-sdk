9cb0cd6af6291ea98b298f7342a2d28eabb6cde2
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-14 19:58:39 +0000

    Create Request and Response Notification Payment

---
+++ src/Http/Payment/Notifications/Request.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace PagSeguro\Http\Payment\Notifications;
+
+use PagSeguro\Http\Request as BaseRequest;
+use PagSeguro\Http\Payment\Notifications\Response;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.9
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Request extends BaseRequest
+{
+    /**
+     * @var string
+     */
+    private $code = '';
+    
+    /**
+     * Get code
+     * 
+     * @retrn string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+    
+    /**
+     * Set code
+     * 
+     * @param string $code
+     * @return $this
+     */
+    public function setCode(string $code)
+    {
+        $this->code = $code;
+        
+        return $this;
+    }
+    
+    /**
+     * Get request method
+     * 
+     * @return string
+     */
+    public function getMethod() : string
+    {
+        return self::GET;
+    }
+    
+    /**
+     * Get request URL
+     * 
+     * @return string
+     */
+    public function getUrl() : string
+    {
+        return $this->env->getUrl() . '/notifications/' . $this->getCode() . '?' . $this->credentials->toString();
+    }
+    
+    /**
+     * Get request headers
+     * 
+     * @return array
+     */
+    public function getHeaders() : array
+    {
+        return [
+            'cache-control: no-cache',
+            'Content-Type: ' . self::JSON,
+            'Accept: application/vnd.pagseguro.com.br.v3+json;charset=ISO-8859-1',
+        ];
+    }
+    
+    /**
+     * Create response
+     * 
+     * @param mixed $data
+     * @param array $info
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function createResponse($data, array $info)
+    {
+        $response = new Response($this->env, 'Notifications');
+        
+        $response->setStatus($info['http_code']);
+        $response->setInfo($info);
+        
+        if ($data === 'Unauthorized') {
+            return $response->setErrors([$data]);
+        }
+        
+        if ($info['http_code'] === 404) {
+            return $response->setErrors(['Not Found']);
+        }
+        
+        return $response->setData($data);
+    }
+}
---
+++ src/Http/Payment/Notifications/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PagSeguro\Http\Payment\Notifications;
+
+use PagSeguro\Http\Response as BaseResponse;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.9
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Response extends BaseResponse
+{
+    /**
+     * Set data
+     * 
+     * @return $this
+     */ 
+    public function setData($data)
+    {
+        $this->data = json_decode($data);
+        
+        return $this;
+    }
+}

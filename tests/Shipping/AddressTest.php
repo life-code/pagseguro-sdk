ec55e075a4572806147dbdc9273c1cdeee92710d
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:14:31 +0000

    Add dependencies

--- tests/Shipping/AddressTest.php
+++ tests/Shipping/AddressTest.php
@@ -2,6 +2,10 @@
 
 use PHPUnit\Framework\TestCase;
 
+use PagSeguro\Contracts\Address as AddressContract;
+use PagSeguro\Shipping\Address;
+use PagSeguro\Exceptions\PagseguroException;
+
 class AddressTest extends TestCase
 {
     // 

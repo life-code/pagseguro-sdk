b12bd0b6fcec24032205f23062e8cdd6b8313ce2
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:32:53 +0000

    Create tests exists and get shipping types

--- tests/Shipping/ShippingTypeTest.php
+++ tests/Shipping/ShippingTypeTest.php
@@ -2,7 +2,28 @@
 
 use PHPUnit\Framework\TestCase;
 
+use PagSeguro\Shipping\Type;
+
 class ShippingTypeTest extends TestCase
 {
-    // 
+    /**
+     * Test exists
+     *
+     * @return void
+     */
+    public function testExists()
+    {
+        $this->assertTrue(Type::exists(Type::TYPE_SEDEX));
+        $this->assertTrue(! Type::exists(10));
+    }
+    
+    /**
+     * Test get type
+     *
+     * @return void
+     */
+    public function testGetTypes()
+    {
+        $this->assertCount(3, Type::getTypes());
+    }
 }

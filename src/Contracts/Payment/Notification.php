06a1f73d09627b16e66793e9db77ed82f37fce38
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:56:26 +0000

    Create Notificatio contract

---
+++ src/Contracts/Payment/Notification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PagSeguro\Contracts\Payment;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.91
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Notification
+{
+    /**
+     * Allows you to view the data of a recurrence by the notification code.
+     * 
+     * @param string $code
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function forCode(string $code);
+}

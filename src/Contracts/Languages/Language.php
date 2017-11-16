fc94e57a85b0c3753b8efe77ec3df1de643bb1ed
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:42:24 +0000

    Create contract for Language

---
+++ src/Contracts/Languages/Language.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PagSeguro\Contracts\Languages;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.91
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Language
+{
+    /**
+     * Generate path
+     * 
+     * @param string $path
+     * @return string
+     */
+    public function generatePath(string $path) : string;
+    
+    /**
+     * Translate error value
+     * 
+     * @param int $code
+     * @param string $default
+     * @return string
+     */
+    public function translate(int $code, string $default = '') : string;
+    
+    /**
+     * Get translations
+     * 
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return array
+     */
+    public function getTranslations() : array
+}

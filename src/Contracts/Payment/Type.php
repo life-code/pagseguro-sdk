c7dce7b52680f33de1f25c122869be4c40b85d58
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 17:00:52 +0000

    Create Type contract

---
+++ src/Contracts/Payment/Type.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PagSeguro\Contracts\Payment;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.91
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Type
+{
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */ 
+    public static function check(string $type) : bool;
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */ 
+    public static function getTypes();
+}

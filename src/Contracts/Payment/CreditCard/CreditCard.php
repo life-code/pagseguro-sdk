05a90c5b45d53a85c7197a0f8bc4e72fb4825ca4
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 17:47:15 +0000

    Create CreditCard contract

---
+++ src/Contracts/Payment/CreditCard/CreditCard.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace PagSeguro\Contracts\Payment;
+
+use PagSeguro\Contracts\Address;
+use PagSeguro\Contracts\Payment\CreditCard\Holder;
+use PagSeguro\Contracts\Payment\CreditCard\Installment;
+use PagSeguro\Exceptions\PagseguroException;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.91
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface CreditCard
+{
+    /**
+     * Get token
+     * 
+     * @return string
+     */
+    public function getToken() : string;
+    
+    /**
+     * Set token
+     * 
+     * @param string $token
+     * @return $this
+     */
+    public function setToken(string $token);
+    
+    /**
+     * Get holder
+     * 
+     * @return string | \PagSeguro\Payment\CreditCard\Holder
+     */
+    public function getHolder();
+    
+    /**
+     * Set holder
+     * 
+     * @param \PagSeguro\Payment\CreditCard\Holder $holder
+     * @return $this
+     */
+    public function setHolder(Holder $holder);
+    
+    /**
+     * Get installment
+     * 
+     * @return string | \PagSeguro\Payment\CreditCard\Installment
+     */
+    public function getInstallment();
+    
+    /**
+     * Set installment
+     * 
+     * @param \PagSeguro\Payment\CreditCard\Installment $installment
+     * @return $this
+     */
+    public function setInstallment(Installment $installment);
+    
+    /**
+     * Get address
+     * 
+     * @return string | \PagSeguro\Contracts\Address
+     */
+    public function getAddress();
+    
+    /**
+     * Set address
+     * 
+     * @param \PagSeguro\Contracts\Address $address
+     * @return $this
+     */
+    public function setAddress(Address $address);
+}

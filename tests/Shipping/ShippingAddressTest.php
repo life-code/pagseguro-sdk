2350637d23785e411c058b400030f459d3a6694b
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:29:22 +0000

    Remove dependency

--- tests/Shipping/ShippingAddressTest.php
+++ tests/Shipping/ShippingAddressTest.php
@@ -4,7 +4,6 @@ use PHPUnit\Framework\TestCase;
 
 use PagSeguro\Contracts\Address as AddressContract;
 use PagSeguro\Shipping\Address;
-use PagSeguro\Exceptions\PagseguroException;
 
 class ShippingAddressTest extends TestCase
 {

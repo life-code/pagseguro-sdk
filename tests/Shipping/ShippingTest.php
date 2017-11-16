1dae1900a4bea4c247c84f124e570647abd924fb
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-16 16:23:12 +0000

    Test instance Shipping

--- tests/Shipping/ShippingTest.php
+++ tests/Shipping/ShippingTest.php
@@ -2,7 +2,27 @@
 
 use PHPUnit\Framework\TestCase;
 
+use PagSeguro\Shipping\Shipping;
+
 class ShippingTest extends TestCase
 {
-    // 
+    /**
+     * Shipping Instance
+     * 
+     * @return \PagSeguro\Contracts\Shipping
+     */
+    public function instance()
+    {
+        return new Shipping();
+    }
+    
+    /**
+     * Test instance
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Shipping::class, $this->instance());
+    }
 }

c5549b42bbe2ade2c5d17c6cacee3de25781b1c3
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-21 16:43:19 +0000

    Create Contract Common classes

---
+++ src/Contracts/Common/Documents.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PagSeguro\Contracts\Common;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Documents
+{
+    /**
+     * @var string
+     */
+    const CPF = 'CPF';
+    
+    /**
+     * Get item
+     * 
+     * @return string
+     */
+    public function getItem($type);
+    
+    /**
+     * Set item
+     * 
+     * @param string $cpf
+     * @param string $item
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return $this
+     */
+    public function setItem(string $type, string $item);
+    
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public function validate(string $type) : bool;
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public function getTypes();
+}
---
+++ src/Contracts/Common/Phone.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PagSeguro\Contracts\Common;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Phone
+{
+    /**
+     * Get code
+     * 
+     * @return string
+     */
+    public function getAreaCode() : string;
+
+    /**
+     * Set code
+     * 
+     * @param string $code
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return $this
+     */
+    public function setAreaCode(string $area_code);
+
+    /**
+     * Get number
+     * 
+     * @return string
+     */
+    public function getNumber() : string;
+
+    /**
+     * Set number
+     * 
+     * @param string $number
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return string
+     */
+    public function setNumber(string $number);
+    
+    /**
+     * To string phone
+     * 
+     * @param bool $mask
+     * @return string
+     */
+    public function toString(bool $mask = true) : string;
+}

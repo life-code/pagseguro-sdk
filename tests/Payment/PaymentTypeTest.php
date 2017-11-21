11f910d590ec0436d09e90c0039f3a8fcfdcf970
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-21 17:10:20 +0000

    Rename file

---
+++ tests/Payment/PaymentTypeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Payment\Type;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PaymentTypeTest extends TestCase
+{
+    /**
+     * Test check
+     *
+     * @return void
+     */
+    public function testCheck()
+    {
+        $this->assertTrue(Type::check(Type::CREDITCARD));
+        $this->assertTrue(! Type::check('CREDIT'));
+    }
+    
+    /**
+     * Test get type
+     *
+     * @return void
+     */
+    public function testGetTypes()
+    {
+        $this->assertCount(1, Type::getTypes());
+    }
+}

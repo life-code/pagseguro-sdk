3a94004e0319e81d0cd97e8455058146be1c4976
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-21 17:10:08 +0000

    Create Common tests

---
+++ tests/Common/DocumentsTest.php
@@ -0,0 +1,74 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Contracts\Common\Documents as DocumentsContract;
+use PagSeguro\Common\Documents;
+use PagSeguro\Exceptions\PagseguroException;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class DocumentsTest extends TestCase
+{
+    /**
+     * Documents Instance
+     * 
+     * @return \PagSeguro\Contracts\Common\Documents
+     */
+    public function instance()
+    {
+        return new Documents();
+    }
+    
+    /**
+     * Test instance
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(DocumentsContract::class, $this->instance());
+    }
+    
+    /**
+     * Test get item
+     *
+     * @return void
+     */
+    public function testGetItem()
+    {
+        $this->assertEquals(
+            '24227052009', 
+            $this->instance()->setItem(DocumentsContract::CPF, '24227052009')->getItem(DocumentsContract::CPF)
+        );
+    }
+    
+    /**
+     * Test set item
+     *
+     * @return void
+     */
+    public function testSetItem()
+    {
+        $this->assertInstanceOf(DocumentsContract::class, $this->instance()->setItem(DocumentsContract::CPF, '24227052009'));
+    }
+    
+    /**
+     * Test throw set item
+     *
+     * @expectedException \PagSeguro\Exceptions\PagseguroException
+     * @return void
+     */
+    public function testThrowSetItem()
+    {
+        $this->instance()->setItem('RG', '24227052009');
+    }
+}

e51b101e6f21717975f6aeb41b382051905728a5
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-22 18:18:23 +0000

    Create Payment Response for simple checkout

--- src/Http/Payment/Response.php
+++ src/Http/Payment/Response.php
@@ -56,4 +56,14 @@ class Response extends BaseResponse
         
         return $response;
     }
+    
+    /**
+     * Redirect for PagSeguro 
+     * 
+     * @return string
+     */
+    public function redirect() : string
+    {
+        return 'https://'.$this->env->getReplace().'pagseguro.uol.com.br/v2/checkout/payment.html?code=' . $this->code;
+    }
 }

107056bbbe40e2f18890f8450a3d3f7b5f61e849
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-24 15:14:05 +0000

    Create get, set and trow email attribute in AccountCredentials

--- tests/Credentials/AccountCredentialsTest.php
+++ tests/Credentials/AccountCredentialsTest.php
@@ -3,6 +3,7 @@
 use PHPUnit\Framework\TestCase;
 
 use PagSeguro\Credentials\AccountCredentials;
+use PagSeguro\Contracts\Credentials\AccountCredentials as AccountCredentialsContract;
 
 /**
  * PagSeguro SDK
@@ -17,12 +18,53 @@ use PagSeguro\Credentials\AccountCredentials;
 class AccountCredentialsTest extends TestCase
 {
     /**
+     * Customer Instance
+     * 
+     * @return \PagSeguro\Contracts\Credentials\AccountCredentials
+     */
+    public static function instance()
+    {
+        return new AccountCredentials();
+    }
+    
+    /**
      * Test instance account credentials
      *
      * @return void
      */
     public function testInstance()
     {
-        $this->assertInstanceOf(AccountCredentials::class, new AccountCredentials());
+        $this->assertInstanceOf(AccountCredentials::class, $this->instance());
+    }
+    
+    /**
+     * Test get email
+     *
+     * @return void
+     */
+    public function testGetEmail()
+    {
+        $this->assertEquals('vinicius_puglies@outlook.com', $this->instance()->setEmail('vinicius_puglies@outlook.com')->getEmail());
+    }
+    
+    /**
+     * Test set email
+     *
+     * @return void
+     */
+    public function testSetEmail()
+    {
+        $this->assertInstanceOf(AccountCredentialsContract::class, $this->instance()->setEmail('vinicius_puglies@outlook.com'));
+    }
+    
+    /**
+     * Test throw set email
+     *
+     * @expectedException \PagSeguro\Exceptions\PagseguroException
+     * @return void
+     */
+    public function testThrowSetEmail()
+    {
+        $this->instance()->setEmail('vinicius_puglies');
     }
 }

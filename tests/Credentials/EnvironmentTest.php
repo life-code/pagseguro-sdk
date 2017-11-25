7bb1db26a41e86bacaddc4d189d3241e9e509240
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-25 23:22:39 +0000

    Create EnvironmentTest.php

---
+++ tests/Credentials/EnvironmentTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Credentials\Environment;
+use PagSeguro\Contracts\Credentials\Environment as EnvironmentContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class EnvironmentTest extends TestCase
+{
+    /**
+     * Customer Instance
+     * 
+     * @return \PagSeguro\Contracts\Credentials\Environment
+     */
+    public static function instance()
+    {
+        return new Environment();
+    }
+    
+    /**
+     * Test instance account credentials
+     *
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Environment::class, $this->instance());
+    }
+}

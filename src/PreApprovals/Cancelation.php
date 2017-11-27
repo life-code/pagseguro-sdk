1ab3c2d9f7f08d8abb6bf2047ab2820849860aed
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-27 17:09:14 +0000

    Create PreApprovals Concelation

---
+++ src/PreApprovals/Cancelation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PagSeguro\PreApprovals;
+
+use PagSeguro\Contracts\Credentials\AccountCredentials;
+use PagSeguro\Contracts\Credentials\Environment;
+use PagSeguro\Contracts\PreApprovals\Cancelation as CancelationContact;
+use PagSeguro\Http\PreApprovals\Cancelations\Request;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Cancelation implements CancelationContact
+{
+    /**
+     * @var \PagSeguro\Contracts\Credentials\AccountCredentials
+     */
+    private $credentials = '';
+    
+    /**
+     * @var \PagSeguro\Contracts\Credentials\Environment
+     */
+    private $env = '';
+    
+    /**
+     * Make new instance of this class.
+     * 
+     * @param \PagSeguro\Contracts\Credentials\AccountCredentials $credentials
+     * @param \PagSeguro\Contracts\Credentials\Environment $env
+     * @return void
+     */
+    public function __construct(AccountCredentials $credentials, Environment $env)
+    {
+        $this->credentials = $credentials;
+        $this->env         = $env;
+    }
+    
+    /**
+     * Allows you to view the data of a recurrence by the notification code.
+     * 
+     * @param string $code
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function forCode(string $code)
+    {
+        $request = new Request($this->credentials, $this->env);
+        
+        return $request->setCode($code)->send();
+    }
+}

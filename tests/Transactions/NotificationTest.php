5a55aa8e84e53b156a90d8661164170d8c55b50c
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-29 15:51:26 +0000

    Create NotificationTest

---
+++ tests/Transactions/NotificationTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\PagSeguro;
+use PagSeguro\Contracts\Transactions\Notification as NotificationContract;
+use PagSeguro\Transactions\Notification;
+use PagSeguro\Contracts\Http\Response;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class NotificationTest extends TestCase
+{
+    /**
+     * Test instance
+     * 
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(NotificationContract::class, PagSeguro::paymentNotification());
+    }
+    
+    /**
+     * Test for code
+     * 
+     * @return void
+     */
+    public function testForCode()
+    {
+        $notification = PagSeguro::paymentNotification();
+        
+        $this->assertInstanceOf(Response::class, $notification->forCode('0BE69413A42B40D998581B9A8732A1CD'));
+    }
+}

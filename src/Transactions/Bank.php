88690b782af68e4b5d1fe72a446ca830b1abf7aa
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-29 12:26:59 +0000

    Craete Bank class

---
+++ src/Transactions/Bank.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PagSeguro\Transactions;
+
+use PagSeguro\Contracts\Transactions\Bank as BankContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Bank implements BankContract
+{
+    /**
+     * @var string
+     */ 
+	const CREDITCARD = 'CREDITCARD';
+	
+    /**
+     * Validate bank
+     * 
+     * @param string $bank
+     * @return bool
+     */ 
+    public static function check(string $bank) : bool
+    {
+        return in_array($bank, self::getBanks());
+    }
+    
+    /**
+     * Get banks
+     * 
+     * @return array
+     */ 
+    public static function getBanks()
+    {
+        return [
+            self::CREDITCARD,
+            self::EFT,
+            self::BOLETO,
+        ];
+    }
+}

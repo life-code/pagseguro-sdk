ae6b88cbec24743cc2f18f21da9ea45eb5cb4038
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-29 12:29:17 +0000

    Create BankTest

---
+++ tests/Transactions/BankTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Transactions\Bank;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class BankTest extends TestCase
+{
+    /**
+     * Test check
+     *
+     * @return void
+     */
+    public function testCheck()
+    {
+        $this->assertTrue(Bank::check(Bank::BANCO_BRASIL));
+        $this->assertTrue(! Bank::check('BRASIL'));
+    }
+    
+    /**
+     * Test get banks
+     *
+     * @return void
+     */
+    public function testGetBanks()
+    {
+        $this->assertCount(1, Bank::getBanks());
+    }
+}

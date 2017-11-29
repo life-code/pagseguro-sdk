b47480f66af9599611caef0e9a59f2f1e0b4c5e9
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-29 15:46:28 +0000

    Create Session contract

---
+++ src/Contracts/Session/Session.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PagSeguro\Contracts\Session;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Session
+{
+    /**
+     * Create Session ID
+     * 
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function create();
+}

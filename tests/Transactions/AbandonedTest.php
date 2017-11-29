fce729563c725638fac8aa167f09f984661a2c10
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-11-29 15:42:44 +0000

    Create AbandonedTest

---
+++ tests/Transactions/AbandonedTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\PagSeguro;
+use PagSeguro\Contracts\Transactions\Abandoned as AbandonedContract;
+use PagSeguro\Transactions\Interval;
+use PagSeguro\Contracts\Http\Response;
+use DateTime;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     0.8.97
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class AbandonedTest extends TestCase
+{
+    /**
+     * Test instance
+     * 
+     * @return void
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(AbandonedContract::class, PagSeguro::abandoned());
+    }
+    
+    /**
+     * Test search
+     * 
+     * @return void
+     */
+    public function testSearch()
+    {
+        $abandoned = PagSeguro::abandoned();
+        
+        $interval = new Interval();
+        $interval->setInitialDate(new DateTime('2017-11-01'));
+        $interval->setFinalDate(new DateTime('2017-11-23'));
+        $interval->setPage(1);
+        $interval->setMaxPageResults(1);
+        
+        $this->assertInstanceOf(Response::class, $abandoned->search($interval));
+    }
+}

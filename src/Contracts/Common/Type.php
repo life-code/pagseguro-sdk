8be40930b73669f6e080626c424083d485bc4e6a
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 18:03:59 +0000

    Move Type contract

---
+++ src/Contracts/Common/Type.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PagSeguro\Contracts\Common;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Type
+{
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public static function check(string $type) : bool;
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes();
+}

f33f7947818bd0ad222ca81fc0520fc039f86bf6
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 18:06:19 +0000

    Create Type class with attrbutes and methods

---
+++ src/PreApprovals/Plan/Type.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PagSeguro\PreApprovals\Plan;
+
+use PagSeguro\Contracts\Common\Type as TypeContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Type implements TypeContract
+{
+    /**
+     * @var string
+     */
+	const AUTOMATIC = 'auto';
+	
+    /**
+     * @var string
+     */
+	const MANUAL = 'manual';
+	
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public static function check(string $type) : bool
+    {
+        return in_array($type, self::getTypes());
+    }
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes()
+    {
+        return [
+            self::AUTO,
+            self::MANUAL,
+        ];
+    }
+}

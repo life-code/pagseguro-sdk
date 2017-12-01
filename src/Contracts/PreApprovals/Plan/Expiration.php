1b8125d4346572444a0307c5000299b28749cb78
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 18:04:48 +0000

    Create Expiration contract

---
+++ src/Contracts/PreApprovals/Plan/Expiration.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PagSeguro\Contracts\PreApprovals\Plan;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Expiration
+{
+    /**
+     * Get value
+     * 
+     * @return int
+     */
+    public function getValue() : int;
+    
+    /**
+     * Set value
+     * 
+     * @param int $value
+     * @return $this
+     */
+    public function setValue(int $value);
+    
+    /**
+     * Get unit
+     * 
+     * @return int
+     */
+    public function getUnit() : int;
+
+    /**
+     * Set unit
+     * 
+     * @param string $unit
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return $this
+     */
+    public function setUnit(string $unit);
+}

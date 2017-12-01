fc7afbb83614aae9572710cb3546b11a5d95e362
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 16:00:48 +0000

    Create Response Plan

---
+++ src/Http/PreApprovals/Plan/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PagSeguro\Http\PreApprovals\Plan;
+
+use PagSeguro\Http\Response as BaseResponse;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Response extends BaseResponse
+{
+    /**
+     * Set data
+     * 
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = json_decode($data);
+        
+        return $this;
+    }
+}

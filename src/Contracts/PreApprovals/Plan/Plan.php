31849bf425babe0e544736cc63dec77286ee1a02
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 18:04:57 +0000

    Create Plan contract

---
+++ src/Contracts/PreApprovals/Plan/Plan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PagSeguro\Contracts\PreApprovals\Plan;
+
+use PagSeguro\PreApprovals\Plan\PreApproval;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+interface Plan
+{
+    /**
+     * Create new plan
+     * 
+     * @param \PagSeguro\PreApprovals\Plan\PreApproval $pre_approval
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function create(PreApproval $pre_approval);
+}

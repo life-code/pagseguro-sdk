b3c5875fd579b77f4dfd88b6a0cecfd6547fe71a
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 16:06:09 +0000

    Added receiver email attribute, get and set methods

--- src/PreApprovals/Plan.php
+++ src/PreApprovals/Plan.php
@@ -6,6 +6,8 @@ use PagSeguro\Contracts\Credentials\AccountCredentials;
 use PagSeguro\Contracts\Credentials\Environment;
 use PagSeguro\Contracts\PreApprovals\Plan as PlanContract;
 use PagSeguro\Http\PreApprovals\Plan\Request;
+use PagSeguro\Exceptions\PagseguroException;
+use PagSeguro\Support\Validator;
 
 /**
  * PagSeguro SDK
@@ -19,6 +21,8 @@ use PagSeguro\Http\PreApprovals\Plan\Request;
  */
 class Plan implements PlanContract
 {
+    use Validator;
+    
     /**
      * @var \PagSeguro\Contracts\Credentials\AccountCredentials
      */
@@ -38,6 +42,11 @@ class Plan implements PlanContract
      * @var string
      */
     private $reference = '';
+    
+    /**
+     * @var string
+     */
+    private $receive_email = '';
     
     /**
      * Make new instance of this class
@@ -94,6 +103,34 @@ class Plan implements PlanContract
     public function setReference(string $reference)
     {
         $this->reference = $reference;
+        
+        return $this;
+    }
+    
+    /**
+     * Get receive email
+     * 
+     * @return string
+     */
+    public function getReceiveEmail() : string
+    {
+        return $this->receive_email;
+    }
+    
+    /**
+     * Set receive email
+     * 
+     * @param string $receive_email
+     * @throws \PagSeguro\Exceptions\PagseguroException
+     * @return $this
+     */
+    public function setReceiveEmail(string $receive_email)
+    {
+        if (!$this->validateEmail($receive_email)) {
+            throw new PagseguroException("The [$receive_email] isn't a valid email.");
+        }
+        
+        $this->receive_email = $receive_email;
         
         return $this;
     }

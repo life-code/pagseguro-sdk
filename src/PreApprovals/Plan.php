ad4149ec807b62839a47a01ce15ce571dc76165a
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-01 15:59:43 +0000

    Create Plan

---
+++ src/PreApprovals/Plan.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PagSeguro\PreApprovals;
+
+use PagSeguro\Contracts\Credentials\AccountCredentials;
+use PagSeguro\Contracts\Credentials\Environment;
+use PagSeguro\Contracts\PreApprovals\Plan as PlanContract;
+use PagSeguro\Http\PreApprovals\Plan\Request;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Plan implements PlanContract
+{
+    /**
+     * Create new plan
+     * 
+     * @return \PagSeguro\Contracts\Http\Response
+     */
+    public function create()
+    {
+        $request = new Request($this->credentials, $this->env);
+        
+        return $request->exchangeData($this)->send();
+    }
+}

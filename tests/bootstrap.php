3959f5c96e85fc8cfd73291dc24adbd2f86f121d
Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
2017-12-03 22:21:40 -0200

    Default comment on bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,14 @@
 <?php
 
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.1
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+
 require __DIR__.'/../vendor/autoload.php';

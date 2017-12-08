079272adad4b352d02c5e2f051b0e724d0eb1667
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-08 19:34:49 +0000

    Create PaymentMode class

---
+++ src/Transactions/PaymentMode.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PagSeguro\Transactions;
+
+use PagSeguro\Contracts\Common\Type as TypeContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.2
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PaymentMode implements TypeContract
+{
+    /**
+     * @var string
+     */
+	const DEFAULT = 'default';
+	
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public static function check(string $type) : bool
+    {
+        return in_array($type, self::getTypes());
+    }
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes() : array
+    {
+        return [
+            self::DEFAULT,
+        ];
+    }
+}

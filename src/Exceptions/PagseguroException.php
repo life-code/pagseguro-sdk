fa3d10197112cd93b7f1c188aaaa9d17da589665
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-08 12:51:10 +0000

    Crete translation method

--- src/Exceptions/PagseguroException.php
+++ src/Exceptions/PagseguroException.php
@@ -3,6 +3,9 @@
 namespace PagSeguro\Exceptions;
 
 use Exception;
+use Throwable;
+use PagSeguro\PagSeguro;
+use PagSeguro\Languages\Language;
 
 /**
  * PagSeguro SDK
@@ -16,5 +19,78 @@ use Exception;
  */
 class PagseguroException extends Exception
 {
-    // 
+    /**
+     * Path to language files
+     * 
+     * @var string
+     */
+    private $path = 'Throws';
+    
+    /**
+     * @var \PagSeguro\Languages\Language
+     */
+    private $language;
+    
+    /**
+     * Make new instance of this class
+     * 
+     * @param string $message
+     * @param int $code
+     * @param \Throwable $previous
+     * @return void
+     */
+    public function __construct(string $message = null, int $code = null, Throwable $previous = null)
+    {
+        $message = $this->handlerMessage($message, $code);
+        
+        parent::__construct($message, $code, $previous);
+    }
+    
+    /**
+     * Handles message
+     * 
+     * @param string $message
+     * @param int $code
+     * @return string
+     */
+    private function handlerMessage(string $message = null, int $code = null) : string
+    {
+        $language = $this->getLanguage();
+        
+        return $language->translate((int) $code, (string) $message);
+    }
+    
+    /**
+     * Get Language instance
+     * 
+     * @return \PagSeguro\Languages\Language
+     */
+    private function getLanguage()
+    {
+        if (!$this->language) {
+            $this->language = new Language($this->getEnv(), $this->getPath());
+        }
+        
+        return $this->language;
+    }
+    
+    /**
+     * Get path
+     * 
+     * @return string
+     */
+    private function getPath() : string
+    {
+        return $this->path;
+    }
+    
+    /**
+     * Get environment
+     * 
+     * @return \PagSeguro\Contracts\Credentials\Environment
+     */
+    private function getEnv()
+    {
+        return Pagseguro::getEnv();
+    }
 }

78d5fcb428b9014938b566a0e055749d427320a3
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-08 18:39:37 +0000

    Create Location

---
+++ src/Languages/Location.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PagSeguro\Languages;
+
+use PagSeguro\Contracts\Common\Type as TypeContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.2
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Location implements TypeContract
+{
+    /**
+     * @var string
+     */
+	const EN = 'en';
+	
+    /**
+     * @var string
+     */
+	const PT_BR = 'pt-br';
+	
+    /**
+     * Validate type
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public static function check(string $type) : bool
+    {
+        return in_array($type, self::getTypes());
+    }
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes() : array
+    {
+        return [
+            self::EN,
+            self::PT_BR,
+        ];
+    }
+}

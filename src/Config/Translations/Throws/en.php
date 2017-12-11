b2fd52ef55b89fa523c80a328a87aaff756c57f5
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-11 15:43:10 +0000

    Address messages translation added

--- src/Config/Translations/Throws/en.php
+++ src/Config/Translations/Throws/en.php
@@ -5,4 +5,5 @@ return [
     '2002' => "The [#value#] isn't a valid document.",
     '2011' => "The [#value#] isn't a valid area code.",
     '2012' => "The [#value#] isn't a valid number.",
+    '2021' => "The [#value#] isn't a valid address country.",
 ];

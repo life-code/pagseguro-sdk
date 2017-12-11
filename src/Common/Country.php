2b8f992167ae427f12c2aa93f0a4d2b9dc9d9131
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-11 15:38:11 +0000

    Create Country class

---
+++ src/Common/Country.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PagSeguro\Common;
+
+use PagSeguro\Contracts\Common\Type as TypeContract;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.2
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class Country implements TypeContract
+{
+    /**
+     * @var int
+     */
+    const BRASIL = 'BRA';
+    
+    /**
+     * Check type exists
+     * 
+     * @param string $type
+     * @return bool
+     */
+    public static function check(string $type) : bool
+    {
+        return in_array($type, self::getTypes());
+    }
+    
+    /**
+     * Get types
+     * 
+     * @return array
+     */
+    public static function getTypes() : array
+    {
+        return [
+            self::BRASIL, 
+        ];
+    }
+}

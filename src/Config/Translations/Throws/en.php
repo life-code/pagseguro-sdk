7d73f2ef60e6f198b513bd61e9133e147ac19b33
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-11 15:49:48 +0000

    Customer messages translation added

--- src/Config/Translations/Throws/en.php
+++ src/Config/Translations/Throws/en.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
-    '2001' => "The [#value#] isn't exists in documents.",
-    '2002' => "The [#value#] isn't a valid document.",
-    '2011' => "The [#value#] isn't a valid area code.",
-    '2012' => "The [#value#] isn't a valid number.",
-    '2021' => "The [#value#] isn't a valid address country.",
+    '2001' => "The document [#value#] isn't exists in documents.",
+    '2002' => "The document [#value#] isn't a valid document.",
+    '2011' => "The area code [#value#] isn't a valid area code.",
+    '2012' => "The number [#value#] isn't a valid number.",
+    '2021' => "The country [#value#] isn't a valid address country.",
     '2101' => "The PagSeguro credential [#value#] isn't a valid email.",
     '2102' => "The PagSeguro credential [#value#] isn't a valid token.",
+    '2201' => "The customer email [#value#] isn't a valid email.",
+    '2202' => "The customer name [#value#] isn't a valid name. Required first and last name",
+    '2203' => "The customer IP [#value#] isn't a valid IP.",
 ];

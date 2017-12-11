03f68f22eaea496eed12e462a69c4090795f1efa
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-11 15:26:38 +0000

    Phone messages translation added

--- src/Config/Translations/Throws/en.php
+++ src/Config/Translations/Throws/en.php
@@ -3,4 +3,6 @@
 return [
     '2001' => "The [#value#] isn't exists in documents.",
     '2002' => "The [#value#] isn't a valid document.",
+    '2011' => "The [#value#] isn't a valid area code.",
+    '2012' => "The [#value#] isn't a valid number.",
 ];

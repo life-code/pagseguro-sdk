1a6a6df0a7372e127a84a67bb96cb0a78b6fdafe
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-13 13:31:15 +0000

    Create PaymentModeTest with methods

---
+++ tests/Transactions/PaymentModeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use PagSeguro\Transactions\PaymentMode;
+
+/**
+ * PagSeguro SDK
+ * 
+ * @type        library
+ * @version     1.0.2
+ * @package     life-code/pagseguro-sdk
+ * @copyright   Copyright (c) 2017 Vinicius Pugliesi (http://www.viniciuspugliesi.com)
+ * @author      Vinicius Pugliesi <vinicius_pugliesi@outlook.com>
+ * @license     MIT
+ */
+class PaymentModeTest extends TestCase
+{
+    /**
+     * Test check
+     *
+     * @return void
+     */
+    public function testCheck()
+    {
+        $this->assertTrue(PaymentMode::check(PaymentMode::DEFAULT));
+        $this->assertTrue(! PaymentMode::check('BRASIL'));
+    }
+    
+    /**
+     * Test get payment modes
+     *
+     * @return void
+     */
+    public function testGetPaymentModes()
+    {
+        $this->assertCount(1, PaymentMode::getTypes());
+    }
+}
